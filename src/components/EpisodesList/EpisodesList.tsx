fb0c7098de10e021eb28a8f416db1a80a6c77c4a
PacoGD <you@example.com>
2023-09-26 12:52:04 +0200

    feat(EpisodeList): create and style table

--- src/components/EpisodesList/EpisodesList.tsx
+++ src/components/EpisodesList/EpisodesList.tsx
@@ -6,6 +6,40 @@ interface EpisodeListProps {
   results: Episode[];
 }
 
+function formatDate(dateISO: Date) {
+  const date = new Date(dateISO);
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Sumamos 1 porque los meses comienzan en 0
+  const year = date.getFullYear();
+
+  return `${day.toString().padStart(2, "0")}/${month
+    .toString()
+    .padStart(2, "0")}/${year}`;
+}
+
+function formatDuration(milliseconds: number) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  let formattedTime = "";
+
+  if (hours > 0) {
+    formattedTime += `${hours.toString().padStart(2, "0")}:`;
+  }
+
+  formattedTime += `${minutes.toString().padStart(2, "0")}:${remainingSeconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  return formattedTime;
+}
+
+function addUrltoTrackName(episode: Episode) {
+  return episode.trackName;
+}
+
 export const EpisodeList: FunctionComponent<EpisodeListProps> = (results) => {
   return (
     <div className={styles.container}>
@@ -23,10 +57,10 @@ export const EpisodeList: FunctionComponent<EpisodeListProps> = (results) => {
           </thead>
           <tbody>
             {results?.results?.map((row, index) => (
-              <tr key={index} className={index % 2 === 0 ? "shaded" : ""}>
-                <td>{row.trackName}</td>
-                <td>{row.releaseDate.toString()}</td>
-                <td>{row.trackTimeMillis}</td>
+              <tr key={index} className={index % 2 === 0 ? styles.shaded : ""}>
+                <td>{addUrltoTrackName(row)}</td>
+                <td>{formatDate(row.releaseDate)}</td>
+                <td>{formatDuration(row.trackTimeMillis)}</td>
               </tr>
             ))}
           </tbody>
