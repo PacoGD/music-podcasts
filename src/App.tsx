d02c6d8ad8d59b4b663f9e52465f78b056791541
PacoGD <you@example.com>
2023-09-28 17:32:02 +0200

    feat: persist store

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,27 @@ import Episode from "./views/EpisodeView/EpisodeView";
 import { Header } from "./components/Header";
 import { LayoutContainer } from "./components/LayoutContainer";
 import { Provider } from "react-redux";
-import { store } from "./state";
+import { persistor, store } from "./state";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <LayoutContainer>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/podcast/:podcastId" element={<Podcast />} />
-            <Route
-              path="/podcast/:podcastId/episode/:episodeId"
-              element={<Episode />}
-            />
-          </Routes>
-        </LayoutContainer>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <LayoutContainer>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/podcast/:podcastId" element={<Podcast />} />
+              <Route
+                path="/podcast/:podcastId/episode/:episodeId"
+                element={<Episode />}
+              />
+            </Routes>
+          </LayoutContainer>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,15 +1,26 @@
+import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import podcastsReducer from "./podcasts/slice";
 import episodeReducer from "./episodes/slice";
 import loadingReducer from "./loading/slice";
 
-export const store = configureStore({
-  reducer: {
-    podcasts: podcastsReducer,
-    episodes: episodeReducer,
-    loading: loadingReducer,
-  },
+const rootReducer = combineReducers({
+  podcasts: podcastsReducer,
+  episodes: episodeReducer,
+  loading: loadingReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
