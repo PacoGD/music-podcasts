d87e6cef814305fe581b12035490c0af8812b70f
PacoGD <you@example.com>
2023-09-28 22:20:41 +0200

    chore: update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const commonOptions = {
+    plugins: [react()],
+  };
+
+  const developmentOptions = {
+    build: {
+      minify: false,
+    },
+  };
+
+  const productionOptions = {
+    build: {
+      minify: true,
+    },
+  };
+
+  return command === "build"
+    ? { ...commonOptions, ...productionOptions }
+    : { ...commonOptions, ...developmentOptions };
+});
